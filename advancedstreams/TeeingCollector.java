b1abed427a3e477791cfc8b3906d07791c472a75
Changed Author <toon.willems@axxes.com>
2022-10-14 11:13:06 +0200

    teeing example

---
+++ advancedstreams/TeeingCollector.java
@@ -0,0 +1,19 @@
+package advancedstreams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TeeingCollector {
+    
+    public static void main(String[] args) {
+        var list = List.of("x",  "y", "z");
+        Seperations result = list.stream()
+            .collect(Collectors.teeing(
+                Collectors.joining(" "),
+                Collectors.joining(" ,"),
+                (s,c) -> new Seperations(s, c)));
+        System.out.println(result);
+    }
+}
+
+record Seperations(String spaceSeperated, String commaSeperated){}
