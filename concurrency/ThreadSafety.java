2e28654cd2fd2a72e4cb61f25acefce15952044b
Changed Author <toon.willems@axxes.com>
2022-10-17 14:52:56 +0200

    Thread safety example

--- concurrency/ThreadSafety.java
+++ concurrency/ThreadSafety.java
@@ -2,22 +2,25 @@ package concurrency;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+
 
 
 public class ThreadSafety {
-    //private volatile int sheepCount = 0;
-    private AtomicInteger sheepCount = new AtomicInteger(0);
+    private int sheepCount = 0;
+    private final Object herd = new Object();
     private void incrementAndReport(){
-        //System.out.print((++sheepCount) + " ");  //NOT THREAD SAFE! Use AtomicInteger to solve double number problem
-        System.out.print(sheepCount.incrementAndGet() + " ");
+        synchronized(herd){
+            System.out.print((++sheepCount) + " ");
+        }
     }
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(20);
         try {
             ThreadSafety manager = new ThreadSafety();
             for (int i = 0; i < 10; i++) {
-                service.submit(() -> manager.incrementAndReport());
+                synchronized(manager){
+                    service.submit(() -> manager.incrementAndReport());
+                }
             } 
         } finally {
             service.shutdown();
