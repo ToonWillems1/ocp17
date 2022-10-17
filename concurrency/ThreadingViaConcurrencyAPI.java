9adacfff30b7e92a7e07a61eda2c14da57ec7f57
Changed Author <toon.willems@axxes.com>
2022-10-17 13:58:08 +0200

    Threading via the concurrency API

---
+++ concurrency/ThreadingViaConcurrencyAPI.java
@@ -0,0 +1,26 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadingViaConcurrencyAPI {
+    public static void main(String[] args) {
+        Runnable printInventory = () -> System.out.println("Printing zoo inventory");
+        Runnable printRecords = () -> {
+           for (int i = 0; i < 3; i++) {
+              System.out.println("Printing record: "+ i);
+           }
+        };
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        try {
+            System.out.println("Begin!");
+            service.execute(printInventory);
+            service.execute(printRecords);
+            service.execute(printInventory);
+            System.out.println("end");
+        } finally{
+            service.shutdown();
+        }
+    }
+}
