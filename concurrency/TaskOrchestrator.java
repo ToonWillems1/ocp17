738de414560ff1861e7cc639d53fe3e0577bcb72
Changed Author <toon.willems@axxes.com>
2022-10-19 08:47:09 +0200

    more concurrency examples

--- concurrency/TaskOrchestrator.java
+++ concurrency/TaskOrchestrator.java
@@ -1,8 +1,9 @@
+package concurrency;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executors;
 
-public class Test{
+public class TaskOrchestrator{
    private void removeLions(){System.out.println("Removing Swakke");}
    private void cleanPen(){System.out.println("Pen is clean");}
    private void addLions(){System.out.println("Swakke toegevoegd");}
@@ -21,7 +22,7 @@ public class Test{
       var service = Executors.newFixedThreadPool(4);
 
       try {
-         var manager = new Test();
+         var manager = new TaskOrchestrator();
          var c1 = new CyclicBarrier(4);
          var c2 = new CyclicBarrier(4,
             () -> System.out.println("*** Pen clean!"));
