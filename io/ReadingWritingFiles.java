71d0f12656b71e7cda362cbb9a49b0bee8e0498d
Changed Author <toon.willems@axxes.com>
2022-10-19 13:44:23 +0200

    IO examples added

--- io/ReadingWritingFiles.java
+++ io/ReadingWritingFiles.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 
 public class ReadingWritingFiles {
@@ -30,5 +33,28 @@ public class ReadingWritingFiles {
                 writer.close(); // close needed to solve data leak
             }
         }
+    }
+
+    void copyPathAsString(Path input, Path output) throws IOException{
+        String string = Files.readString(input);
+        Files.writeString(output, string);
+    }
+
+    void copyPathAsBytes(Path input, Path output) throws IOException {
+        byte[] bytes = Files.readAllBytes(input);
+        Files.write(output, bytes);
+    }
+
+    void copyPathAsLines(Path input, Path output) throws IOException {
+        List<String> lines = Files.readAllLines(input);
+        Files.write(output, lines);
+    }
+    //possible OutOfMemoryError with the above three methods, solution below
+    void readLazily(Path path) throws IOException {
+        try(var s = Files.lines(path)){
+            s.filter(f -> f.startsWith("WARN:"))
+                .map(f -> f.substring(5))
+                .forEach(System.out::println);
+        }        
     }
 }
